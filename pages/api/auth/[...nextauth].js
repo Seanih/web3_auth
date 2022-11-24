87a9ff2936516ab2876c31f64377d640f6e06676
Sean Anih <sean.anih@gmail.com>
2022-11-23 20:32:40 -0500

    connected dapp to MongoDB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
 import NextAuth from 'next-auth';
 import Moralis from 'moralis';
+import connectDB from '../../../lib/connectDB';
+import Users from '../../../lib/userSchema';
 
 export default NextAuth({
 	providers: [
@@ -31,6 +33,18 @@ export default NextAuth({
 					).raw;
 
 					const user = { address, profileId, signature };
+
+					//
+					await connectDB();
+					const MongoUser = await Users.findOne({ profileId });
+
+					if (!MongoUser) {
+						const newUser = new Users({
+							profileId,
+						});
+
+						await newUser.save();
+					}
 					// returning the user object and creating  a session
 					return user;
 				} catch (e) {
